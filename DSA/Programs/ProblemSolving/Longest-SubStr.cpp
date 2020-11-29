d73f286e2872cc4888b850fb2d8336732bd7a38e
rvp <varunrajput11@zoho.com>
2020-11-29 18:52:49 +0530

    Longest Common Substring CPP

--- DSA/Programs/ProblemSolving/Longest-SubStr.cpp
+++ DSA/Programs/ProblemSolving/Longest-SubStr.cpp
@@ -1,3 +1,11 @@
 // Q. https://practice.geeksforgeeks.org/problems/longest-common-substring/0
 // A. https://www.geeksforgeeks.org/longest-common-substring-dp-29/
+// A. https://www.youtube.com/watch?v=UZRkpGk943Q
 
+#include<iostream> 
+#include<string.h> 
+using namespace std; 
+
+int LCS(char *X,char *Y,int m,int n){
+
+}
--- DSA/Programs/test.py
+++
@@ -1,6 +0,0 @@
-import json
-f = open('example_1.json',)
-data = json.load(f)
-for i in data['emp_details']:
-    print(i['emp_name'])
-f.close()
