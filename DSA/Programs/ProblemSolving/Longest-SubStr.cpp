95c3e5cbd2eb712dc45ce4192b554dca1ac60030
rvp <varunrajput11@zoho.com>
2020-11-29 18:24:09 +0530

    update

---
+++ DSA/Programs/ProblemSolving/Longest-SubStr.cpp
@@ -0,0 +1,3 @@
+// Q. https://practice.geeksforgeeks.org/problems/longest-common-substring/0
+// A. https://www.geeksforgeeks.org/longest-common-substring-dp-29/
+
--- Tools/MAC Address Changer/mac_changer.py
+++ Tools/MAC Address Changer/mac_changer.py
@@ -16,13 +16,17 @@ def get_args():
         parser.error("Please specify an Interface, use --help for more info.")
     elif not options.new_mac:
         parser.error("Please specify a MAC Address, use --help for more info.")
-    
+
     return options
 
 options = get_args()
-change_mac(options.interface,options.new_mac)
+# change_mac(options.interface,options.new_mac)
 
-# Insecure Way, prone to Injections
+#Reading the output from the ifconfig command with the reqd interface
+res = subprocess.check_output(["ifconfig",options.interface])
+
+
+### Subprocess Call - Insecure Way, prone to Injections
 # subprocess.call("ifconfig" + interface + " down", shell=True)
 # subprocess.call("ifconfig" + interface + " hw ether" + new_mac, shell=True)
 # subprocess.call("ifconfig" + interface + " up", shell=True)
