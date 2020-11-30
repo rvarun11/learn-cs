90fc268327470af0be15855dbffd48486796352f
rvp <varunrajput11@zoho.com>
2020-11-30 12:40:00 +0530

    Updates

--- DSA/Notes/DP.md
+++ DSA/Notes/DP.md
@@ -2,4 +2,4 @@
 
 ##### [Def.]
     - It is an algorithmic technique used to optimize a problem by breaking it down into smaller chunks.
-    - It is based on the idea that an optimized solution is made up of optimized sub-solutions. 
+    - It is based on the idea that an optimized solution is made up of optimized sub-solutions.
---
+++ Misc/graph.py
@@ -0,0 +1,26 @@
+# This is made for educational purposes. Please do not use this to gain unfair advantage over others.
+import os
+import random
+
+
+def commitDays(days):
+    x = random.randint(0,1)
+    if x == 0:
+        n = random.randrange(3)
+        task(n,days)
+    else:
+        n = random.randrange(5,8)
+        task(n,days)
+    
+
+def task(days,n):
+    for i in range(days):
+        cd = str(i) + 'days ago'
+        while(n!=0):
+            with open('test.txt','a') as file:
+                file.write(cd+'\n')
+            os.system('git add test.txt')
+            os.system('git commit --date="'+cd+'"-m "updates"')
+            n-=1
+
+    os.system('git push -u origin master')
