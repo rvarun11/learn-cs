873fcd1cf865b44a1354f4f94be37b14f0956b77
rvp <varunrajput11@zoho.com>
2020-12-19 01:33:08 +0530

    added unique nos. self solution

--- DSA/Programs/ProblemSolving/Codeforces/UniqueNo.cpp
+++ DSA/Programs/ProblemSolving/Codeforces/UniqueNo.cpp
@@ -3,31 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-  int x;
-  cin >> x;
-  vector<int> ans;
-  int sum = 0, last = 9;
-  while (sum < x && last > 0) {
-    ans.push_back(min(x - sum, last));
-    sum += last;
-    last--;
+void solve(int x) {
+ int sum=0,last=9;
+ vector<int> ans;
+ while (sum<x && last>0){
+   int a = x-sum;
+   ans.push_back(min(a,last));
+   sum+=last;
+   last--;
+ }
+
+ if (sum < x)
+  cout<<-1<<endl;
+else{
+  reverse(ans.begin(),ans.end());
+  for (int i: ans){
+    cout << i;
   }
-  if (sum < x) {
-    cout << -1 << "\n";
-  } else {
-    reverse(ans.begin(), ans.end());
-    for (int i : ans) {
-      cout << i;
-    }
-    cout << "\n";
-  }
+  cout << endl;
+}
 }
 
 int main() {
   int t;
   cin >> t;
   while (t--) {
-    solve();
+    int x;
+    cin >> x;
+    solve(x);
   }
 }
