8b7caee56b634db5e1860a8bcc0c85baa282221e
rvp <varunrajput11@zoho.com>
2020-12-21 00:43:49 +0530

    CF Technocup 2021 Div3 Prob A

---
+++ DSA/Programs/ProblemSolving/Codeforces/FairNumbers.cpp
@@ -0,0 +1,27 @@
+//
+#include<bits/stdc++.h>
+using namespace std;
+#define L long long int
+void solve(L n){
+    L temp = n;
+    while (n){
+        int d = n%10;
+        if (temp%d!=0){
+            temp++;
+            n++;
+        }
+        n/=10;
+    }
+
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        L n;
+        cin>>n;
+        solve(n);
+    }
+    return 0;
+}
