7965082fdc0916166e2010a5d3c000a14a1208e9
varun rajput <varunrajput11@zoho.com>
2021-01-02 12:26:19 +0530

    added DIVTHREE C

--- DSA/Programs/ProblemSolving/test.cpp
+++ DSA/Programs/ProblemSolving/test.cpp
@@ -1,8 +1,28 @@
-#include<iostream>
+// https://www.codechef.com/JAN21C?order=desc&sortBy=successful_submissions
 #include<bits/stdc++.h>
 using namespace std;
+#define l long long int
 
 int main(){
-    cout << "Hello World" << endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k,d;
+        cin>>n>>k>>d;
+        l x,cp=0;
+        for(int i=0;i<n;i++){
+            cin>>x;
+            cp+=x;
+        }
+        if(cp<k)
+            cout<<0<<endl;
+        else{
+            int c=cp/k;
+            if (c<=d)
+                cout<<c<<endl;
+            else
+                cout<<d<<endl;
+        }
+    }
     return 0;
 }
