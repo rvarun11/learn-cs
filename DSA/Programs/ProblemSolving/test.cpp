311bca7adb1686eaa1d950b373fb9ee606e14ae1
varun rajput <varunrajput11@zoho.com>
2021-01-04 10:54:46 +0530

    Added archive

--- DSA/Programs/ProblemSolving/test.cpp
+++ DSA/Programs/ProblemSolving/test.cpp
@@ -3,43 +3,40 @@
 using namespace std;
 #define L long long int
 
-int binToDec(int b){
-    int dec_value = 0, base = 1;
- 
-    int temp = b;
-    while (temp) {
-        int last_digit = temp % 10;
-        temp = temp / 10;
- 
-        dec_value += last_digit * base;
- 
-        base = base * 2;
-    }
- 
-    return dec_value;
-}
-
-char solve(string s){
-    stringstream ss(s);
-    int y=0;
-    ss>>y;
-    y = binToDec(y);
-    return (97+y);
-}
-
 int main(){
     int t;
     cin>>t;
     while(t--){
-        L n;
-        cin>>n;
-        string x;
-        cin>>x;
-        for (int i=0;i<n;i+=4){
-            string sub = x.substr(i,4);
-            cout<<solve(sub);
+        int n,m;
+        cin>>n>>m;
+        vector<int> arr,brr;
+        L suma=0,sumb=0;
+        for (int i=0;i<n;i++){
+            L x;
+            cin>>x;
+            suma+=x;
+            arr.push_back(x);
         }
-        cout<<endl;
+        for (int i=0;i<m;i++){
+            L x;
+            cin>>x;
+            sumb+=x;
+            brr.push_back(x);
+        }
+        if (suma>sumb){
+            cout<<0<<endl;
+            break;
+        }
+        int s=0;
+        while(suma<=sumb){
+            L mina=*min_element(arr.begin(),arr.end());
+            L maxb=*max_element(brr.begin(),brr.end());
+    
+        }
+
+        }
+
+
     }
     return 0;
 }
