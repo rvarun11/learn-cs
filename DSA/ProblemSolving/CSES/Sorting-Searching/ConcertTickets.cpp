27c28175060a629bee3116d795ed2b61384d3753
varun rajput <varunrajput11@zoho.com>
2021-01-16 15:57:03 +0530

    inc ConcertTickets

--- DSA/ProblemSolving/CSES/Sorting-Searching/ConcertTickets.cpp
+++ DSA/ProblemSolving/CSES/Sorting-Searching/ConcertTickets.cpp
@@ -5,7 +5,23 @@ using namespace std;
 #define ll long long int
 
 int main(){
-    int n,m;
+    int n,m,temp;
+    cin>>n>>m;
+    vector<int> tickets(n);
+    for (int i=0;i<n;i++)
+        cin>>tickets[i];
+    
+    sort(tickets.rbegin(),tickets.rend());
 
+    for(int i=0;i<m;i++){
+        cin>>temp;
+        auto it=lower_bound(tickets.begin(),tickets.end(),temp);
+        if (it==tickets.end())
+                cout<<"-1"<<"\n";
+        else{
+            cout<<*it<<"\n";
+            tickets.erase(it);
+        }
+    }
     return 0;
 }
--- DSA/ProblemSolving/test.cpp
+++ DSA/ProblemSolving/test.cpp
@@ -1,10 +1,22 @@
-//
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define lli long long int
+#define li long int
+#define ld long double
 using namespace std;
-#define ll long long int
+const lli mod = 1e9 + 7;
 
-int main(){
-    
+int main()
+{
+    vector<int> v={5,4,3,2,1};
+    multiset<int,greater<int>> s;
+    s.insert(5);
+    s.insert(4);
+    s.insert(3);
+    s.insert(2);
+    s.insert(1);
+    auto i1 = lower_bound(v.begin(),v.end(),2);
+    auto i2 = s.lower_bound(2);
+    cout<<*i1<<" "<<*i2<<endl;
+
     return 0;
 }
