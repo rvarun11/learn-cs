31a32e4e983c05e157787fe8138e12e50e8de671
varun rajput <varunrajput11@zoho.com>
2021-01-27 10:12:57 +0530

    update Playlist

--- DSA/ProblemSolving/CSES/Sorting-Searching/Playlist.cpp
+++ DSA/ProblemSolving/CSES/Sorting-Searching/Playlist.cpp
@@ -8,20 +8,21 @@ using namespace std;
 // const lli mod = 1e9 + 7;
 
 int main(){
-    int n,x;
+    lli n,x;
     cin>>n;
-    unordered_set<int> songs;
-    int count = 0,max_seq=0;
-    for (int i=0;i<n;i++){
-        cin>>x;
-        if (songs.find(x)!=songs.end()){
-            count=0;
-            songs.clear();
+    vector<lli> songs(n);
+    
+    for (int i=0;i<n;i++)
+        cin>>songs[i];
+    int i=0,j=1;
+    lli count=0,max_seq=0;
+    while(songs.size()!=0){
+        if (songs[i]==songs[j]){
+            i++;
         }
-        songs.insert(x);
-        count++;
-        max_seq = max(max_seq,count);
     }
+        
+
     cout<<max_seq;
 	return 0;
 }
