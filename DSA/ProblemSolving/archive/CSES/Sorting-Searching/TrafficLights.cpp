da6e3d416d6b0a0b240b86fd0badb0045c563856
varun rajput <varunrajput11@zoho.com>
2021-01-28 19:10:28 +0530

    updated DIR

---
+++ DSA/ProblemSolving/archive/CSES/Sorting-Searching/TrafficLights.cpp
@@ -0,0 +1,19 @@
+//https://cses.fi/problemset/task/1163
+
+#include <bits/stdc++.h>
+#define lli long long int
+#define li long int
+#define ld long double
+using namespace std;
+
+// const lli mod = 1e9 + 7;
+
+int main(){
+    int x,n;
+    cin>>x>>n;
+    vector<int> ans;
+    for (int i=0;i<n;i++){
+        cin>>
+    }
+    return 0;
+}
