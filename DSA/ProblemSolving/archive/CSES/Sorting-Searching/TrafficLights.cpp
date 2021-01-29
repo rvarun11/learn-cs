58de5624330417ab6f001f00481c1612528171d4
varun rajput <varunrajput11@zoho.com>
2021-01-29 13:31:18 +0530

    added approach to TrafficLights

--- DSA/ProblemSolving/archive/CSES/Sorting-Searching/TrafficLights.cpp
+++ DSA/ProblemSolving/archive/CSES/Sorting-Searching/TrafficLights.cpp
@@ -9,11 +9,38 @@ using namespace std;
 // const lli mod = 1e9 + 7;
 
 int main(){
-    int x,n;
+    int x,n,point,l,r;
     cin>>x>>n;
-    vector<int> ans;
+    set<int> locations = {0,x};
+    multiset<int> distances;
+    
     for (int i=0;i<n;i++){
-        cin>>
+        cin>>point;
+        
+        auto it = locations.upper_bound(point);
+        l = *prev(it);
+        r = *it;
+        locations.insert(point);
+
+        auto e = distances.find(r-l);
+        if (e!=distances.end())
+            distances.erase(e);
+        
+        distances.insert(point-l);
+        distances.insert(r-point);
+
+        cout<<*distances.rbegin()<<" ";
     }
     return 0;
 }
+
+/*
+Let their be a set, locations, initialized to {0,x} which will store locations of all the points, and a 
+multiset distances which will store distances between all point.
+
+For each input do the following:
+    i.   Find elements to the left and right of that point.
+    ii.  Erase the range (right-left) if it exists. [so that if when point is added between that range, that range doesnt exist anymore]
+    iii. Insert the distances (point - left) and (right - point) to distances.
+    iv.  Print the right most element in distances.
+*/
